123560d74eea77a2b2f2e3ee694efd0ab554fcc5
Eda-Guo <guoyitong2018@gmail.com>
2020-12-29 18:26:16 -0800

    1st submit proj1a

---
+++ proj1a/AarrayDeque.java
@@ -0,0 +1,102 @@
+public class AarrayDeque<T> {
+    private static int initialCapacity = 8;
+    private static int factor = 2;
+    private T[] items;
+    private int capacity;
+    private int size;
+    private double ratio;
+    public AarrayDeque(){
+        items = (T[])new Object[initialCapacity];
+        size = 0;
+        capacity = initialCapacity;
+        ratio = 0.25;
+    }
+
+    private void resize(int capacity){
+        T[] nItems = (T[]) new Object[capacity];
+        System.arraycopy(items, 0, nItems, 0, size);
+        this.capacity = capacity;
+        items = nItems;
+    }
+    public boolean isEmpty(){
+        return size == 0;
+    }
+    public int size(){
+        return size;
+    }
+    public void addFirst(T item){
+        if (size == items.length){
+            resize(size * factor);
+        }
+        System.arraycopy(items, 0, items, 1, size);
+        items[0] = item;
+        size++;
+
+    }
+
+    public void addLast(T item){
+        if (size == items.length){
+            resize(size * 2);
+        }
+        items[size] = item;
+        size++;
+    }
+    public void printDeque(){
+        for (int i = 0; i < size; i++){
+            System.out.print(items[i] + " ");
+        }
+        System.out.println();
+    }
+    private void checkRatio(){
+        if ((double)size / capacity < ratio){
+            resize(capacity / factor);
+        }
+    }
+    public T removeFirst(){
+        if (isEmpty()){
+            return null;
+        }
+        T res = items[0];
+        System.arraycopy(items, 1, items, 0, size - 1);
+        items[size - 1]  = null;
+        size--;
+        checkRatio();
+        return res;
+    }
+    public T removeLast(){
+        if (isEmpty()){
+            return null;
+        }
+        T res = items[size];
+        items[size - 1]  = null;
+        size--;
+        checkRatio();
+        return res;
+    }
+
+    public T get(int index){
+        if (index >= size){
+            return null;
+        }
+        return items[index];
+    }
+
+//    public  static void main(String[] args){
+//        AarrayDeque<Integer> ad = new AarrayDeque<>();
+//        ad.addFirst(1);
+//        ad.addFirst(2);
+//        ad.addFirst(3);
+//        ad.addFirst(4);
+//        ad.addFirst(5);
+//        ad.addFirst(6);
+//        ad.addFirst(7);
+//        ad.addLast(8);
+//        ad.addLast(9);
+//        ad.removeFirst();
+//        ad.removeLast();
+//        System.out.println(ad.get(3));
+//        ad.printDeque();
+//        ad.printDeque();
+//    }
+
+}
--- proj1a/LinkedListDequeTest.java
+++ proj1a/LinkedListDequeTest.java
@@ -35,9 +35,10 @@ public class LinkedListDequeTest {
 	  * && is the "and" operation. */
 	public static void addIsEmptySizeTest() {
 		System.out.println("Running add/isEmpty/Size test.");
-		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-		/*
+//		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+
 		LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
+
 
 		boolean passed = checkEmpty(true, lld1.isEmpty());
 
@@ -57,8 +58,10 @@ public class LinkedListDequeTest {
 		System.out.println("Printing out deque: ");
 		lld1.printDeque();
 
+		//System.out.println(lld1.getRecursive(2));
+
 		printTestStatus(passed);
-		*/
+
 	}
 
 	/** Adds an item, then removes an item, and ensures that dll is empty afterwards. */
@@ -66,10 +69,10 @@ public class LinkedListDequeTest {
 
 		System.out.println("Running add/remove test.");
 
-		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-		/*
+//		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+
 		LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
-		// should be empty 
+		// should be empty
 		boolean passed = checkEmpty(true, lld1.isEmpty());
 
 		lld1.addFirst(10);
@@ -81,7 +84,7 @@ public class LinkedListDequeTest {
 		passed = checkEmpty(true, lld1.isEmpty()) && passed;
 
 		printTestStatus(passed);
-		*/
+
 	}
 
 	public static void main(String[] args) {
